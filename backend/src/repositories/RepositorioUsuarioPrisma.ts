7570cf1ae1d82ab8f111c68c4b8bdaec876caf7c
diegomaustem <diego.maustem@gmail.com>
2025-08-09 16:09:54 -0300

    :sparkles: feat: Adicionando repositório usuário

---
+++ backend/src/repositories/RepositorioUsuarioPrisma.ts
@@ -0,0 +1,94 @@
+import { IPaginacao } from "../interfaces/IPaginacao";
+import { IUsuario } from "../interfaces/Usuario/IUsuario";
+import { IResultadoPaginado } from "../interfaces/IResultadoPaginado";
+import { IUsuarioRepository } from "../interfaces/Usuario/IUsuarioRepository";
+import prisma from "../lib/prismaClient";
+
+export class RepositorioUsuarioPrisma implements IUsuarioRepository {
+  async listarUsuarios(
+    paginacao: IPaginacao
+  ): Promise<IResultadoPaginado<IUsuario>> {
+    const { pagina, limite } = paginacao;
+    try {
+      const [dados, total] = await Promise.all([
+        prisma.usuario.findMany({
+          skip: (pagina - 1) * limite,
+          take: limite,
+          orderBy: { id: "asc" },
+        }),
+        prisma.usuario.count(),
+      ]);
+
+      return {
+        dados,
+        paginacao: {
+          total,
+          pagina,
+          limite,
+          totalPaginas: Math.ceil(total / limite),
+        },
+      };
+    } catch (error) {
+      console.error("[Repository] - Erro ao buscar usuários.", error);
+      throw error;
+    }
+  }
+
+  async listarUsuario(id: number): Promise<IUsuario | null> {
+    try {
+      return await prisma.usuario.findUnique({
+        where: { id },
+      });
+    } catch (error) {
+      console.error(`[Repository] - Erro ao buscar usuário ID ${id}`, error);
+      throw error;
+    }
+  }
+
+  async criarUsuario(usuario: IUsuario): Promise<IUsuario> {
+    try {
+      return await prisma.usuario.create({
+        data: usuario,
+      });
+    } catch (error) {
+      console.error("[Repository] - Erro ao criar usuário.", error);
+      throw error;
+    }
+  }
+
+  async atualizarUsuario(id: number, usuario: IUsuario): Promise<IUsuario> {
+    try {
+      return await prisma.usuario.update({
+        where: { id },
+        data: usuario,
+      });
+    } catch (error) {
+      console.error("[Repository] - Erro ao atualizar usuário.", error);
+      throw error;
+    }
+  }
+
+  async excluirUsuario(id: number): Promise<IUsuario> {
+    try {
+      return await prisma.usuario.delete({
+        where: { id },
+      });
+    } catch (error) {
+      console.error("[Repository]- Erro ao excluir usuário.", error);
+      throw error;
+    }
+  }
+
+  async encontrarUsuarioPorLogin(login: string): Promise<IUsuario | null> {
+    try {
+      return await prisma.usuario.findFirst({
+        where: {
+          login: login,
+        },
+      });
+    } catch (error) {
+      console.error("[Repository] - Erro ao buscar usuário por login.", error);
+      throw error;
+    }
+  }
+}
