0cce8dce3e2778ee0ee8fce471bd9340e1a8536e
diegomaustem <diego.maustem@gmail.com>
2025-08-09 10:19:11 -0300

    :sparkles: feat: Criação de repositório para categoria

---
+++ backend/src/repositories/RepositorioCategoriaPrisma.ts
@@ -0,0 +1,47 @@
+import { IPaginacao } from "../interfaces/IPaginacao";
+import { ICategoria } from "../interfaces/ICategoria";
+import { ICategoriaRepository } from "../interfaces/ICategoriaRepository";
+import { IResultadoPaginado } from "../interfaces/IResultadoPaginado";
+import prisma from "../lib/prismaClient";
+
+export class RepositorioCategoriaPrisma implements ICategoriaRepository {
+  async listarCategorias(
+    paginacao: IPaginacao
+  ): Promise<IResultadoPaginado<ICategoria>> {
+    const { pagina, limite } = paginacao;
+    try {
+      const [dados, total] = await Promise.all([
+        prisma.categoria.findMany({
+          skip: (pagina - 1) * limite,
+          take: limite,
+          orderBy: { id: "asc" },
+        }),
+        prisma.categoria.count(),
+      ]);
+
+      return {
+        dados,
+        paginacao: {
+          total,
+          pagina,
+          limite,
+          totalPaginas: Math.ceil(total / limite),
+        },
+      };
+    } catch (error) {
+      console.error("[Repository] - Erro ao buscar categorias.", error);
+      throw error;
+    }
+  }
+
+  async listarCategoria(id: number): Promise<ICategoria | null> {
+    try {
+      return await prisma.categoria.findUnique({
+        where: { id },
+      });
+    } catch (error) {
+      console.error(`[Repository] - Erro ao buscar categoria ID ${id}`, error);
+      throw error;
+    }
+  }
+}
