a4b22171712ad4ea3691e9d3ca94cbf9b535ef4a
diegomaustem <diego.maustem@gmail.com>
2025-08-10 11:56:17 -0300

    :sparkles: feat: Adicionando os validations para inserção e atualização usuário

--- backend/src/routes/modules/usuarioRoutes.ts
+++ backend/src/routes/modules/usuarioRoutes.ts
@@ -1,12 +1,26 @@
 import { Router } from "express";
-import { usuarioController } from "../../controllers/UsuarioController";
+import { criarUsuarioController } from "../../composition/usuarios.composition";
+import { validate } from "../../middlewares/middlewareValidacao";
+import {
+  schemaAtualizacaoUsuario,
+  schemaCriacaoUsuario,
+} from "../../validations/ValidacaoUsuario";
 
 const router = Router();
+const usuarioController = criarUsuarioController();
 
 router.get("/", usuarioController.listarUsuarios);
 router.get("/:id", usuarioController.listarUsuario);
-router.post("/", usuarioController.criarUsuario);
-router.patch("/:id", usuarioController.atualizarUsuario);
+router.post(
+  "/",
+  validate(schemaCriacaoUsuario, "body"),
+  usuarioController.criarUsuario
+);
+router.patch(
+  "/:id",
+  validate(schemaAtualizacaoUsuario, "body"),
+  usuarioController.atualizarUsuario
+);
 router.delete("/:id", usuarioController.excluirUsuario);
 
 export const usuarioRoutes = router;
