2735191cd7a46089ca5227350ca7d5f1fd54b9df
diegomaustem <diego.maustem@gmail.com>
2025-08-10 18:52:12 -0300

    :sparkles: feat: Criação de autenticação nas rotas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,14 @@ import { categoriaRoutes } from "./modules/categoriaRoutes";
 import { usuarioRoutes } from "./modules/usuarioRoutes";
 import { receitaRoutes } from "./modules/receitaRoutes";
 import { autenticacaoRoutes } from "./modules/autenticacaoRoutes";
+import { auth } from "../middlewares/middlewareAutenticacao";
+import { limitadorRequisicao } from "../middlewares/limitadorRequisicao";
 
 const router = Router();
 
-router.use("/usuarios", usuarioRoutes);
-router.use("/categorias", categoriaRoutes);
-router.use("/receitas", receitaRoutes);
-router.use("/login", autenticacaoRoutes);
+router.use("/usuarios", auth, usuarioRoutes);
+router.use("/categorias", auth, categoriaRoutes);
+router.use("/receitas", auth, receitaRoutes);
+router.use("/login", limitadorRequisicao, autenticacaoRoutes);
 
 export default router;
