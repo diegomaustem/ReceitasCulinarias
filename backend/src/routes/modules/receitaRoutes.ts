e12633b5e5b8e7c5ddfd41491c6d422711659970
diegomaustem <diego.maustem@gmail.com>
2025-08-10 14:15:41 -0300

    :sparkles: feat: Criação de rotas com schema de validação receita

--- backend/src/routes/modules/receitaRoutes.ts
+++ backend/src/routes/modules/receitaRoutes.ts
@@ -1,13 +1,26 @@
 import { Router } from "express";
 import { criarReceitaController } from "../../composition/receita.composition";
+import { validate } from "../../middlewares/middlewareValidacao";
+import {
+  schemaAtualizacaoReceita,
+  schemaCriacaoReceita,
+} from "../../validations/ValidacaoReceita";
 
 const router = Router();
 const receitaController = criarReceitaController();
 
 router.get("/", receitaController.listarReceitas);
 router.get("/:id", receitaController.listarReceita);
-router.post("/", receitaController.criarReceita);
-router.patch("/:id", receitaController.atualizarReceita);
+router.post(
+  "/",
+  validate(schemaCriacaoReceita, "body"),
+  receitaController.criarReceita
+);
+router.patch(
+  "/:id",
+  validate(schemaAtualizacaoReceita, "body"),
+  receitaController.atualizarReceita
+);
 router.delete("/:id", receitaController.excluirReceita);
 
 export const receitaRoutes = router;
