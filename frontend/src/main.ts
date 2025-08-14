3584f4ca3c80710c19250c2e373bdd37b0b6f99f
diegomaustem <diego.maustem@gmail.com>
2025-08-13 23:05:45 -0300

    :wrench: chore: Config arquivo do bootstrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { createPinia } from "pinia";
 import router from "./router";
 import App from "./App.vue";
